d278db52bb699aa981a1a1cbc977946e1e215957
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-04-04 10:28:38 -0400

    generate money tree dynamically using JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const main = () =>
+{
+    //VARIABLES
+    const moneyTreeRound1 = [0,1000,5000,10000,50000,75000,125000,250000,500000]
+    //let moneyTreeItem = ``;
+    let currentMoneyTreeIndex = 0;
+    
+    //TARGET ELEMENTS
+    const moneyTreeNode = document.querySelector(".money-tree-container")
+
+    //GENERATE THE MONEY TREE DYNAMICALLY
+    for(let i=moneyTreeRound1.length-1; i>=0; i--)
+    {
+        currentMoneyTreeIndex = i
+        moneyTreeNode.innerHTML += `<div class="grid money-tree-item">${moneyTreeRound1[i]}</div>`;
+        
+    }
+
+
+
+
+}
+main();
